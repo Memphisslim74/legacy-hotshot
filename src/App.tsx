8dfc9b8d5a7fb45c1ac3804bf4001f88557debb0
Steve Smith <150203199+Memphisslim74@users.noreply.github.com>
2026-07-29 21:20:56 -0500

    Add role-protected driver portal routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import { DocumentsPage } from './pages/DocumentsPage'
 import { QuotesPage } from './pages/QuotesPage'
 import { PublicLoadRequestPage } from './pages/PublicLoadRequestPage'
 import { CompanySettingsPage } from './pages/CompanySettingsPage'
+import { DriverPortalPage } from './pages/DriverPortalPage'
+import { DriverLoadPage } from './pages/DriverLoadPage'
 
 function LoadingScreen() {
   return <div className="loading-screen"><div className="loading-mark">L</div><span>Loading Command Center...</span></div>
@@ -24,7 +26,14 @@ function ProtectedLayout() {
   if (loading) return <LoadingScreen />
   if (!user) return <Navigate to="/login" replace state={{ from: location.pathname }} />
   if (!user.setupComplete) return <Navigate to="/setup" replace />
+  if (user.role === 'driver' && location.pathname === '/') return <Navigate to="/driver" replace />
   return <AppShell><Outlet /></AppShell>
+}
+
+function OfficeOnly() {
+  const { user } = useAuth()
+  if (user?.role === 'driver') return <Navigate to="/driver" replace />
+  return <Outlet />
 }
 
 export default function App() {
@@ -34,20 +43,24 @@ export default function App() {
       <Route path="/setup" element={<SetupPage />} />
       <Route path="/request-load" element={<PublicLoadRequestPage />} />
       <Route element={<ProtectedLayout />}>
-        <Route index element={<DashboardPage />} />
-        <Route path="loads" element={<LoadsPage />} />
-        <Route path="loads/new" element={<NewLoadPage />} />
-        <Route path="loads/:id" element={<LoadDetailPage />} />
-        <Route path="quotes" element={<QuotesPage />} />
-        <Route path="customers" element={<CustomersPage />} />
+        <Route path="driver" element={<DriverPortalPage />} />
+        <Route path="driver/loads/:id" element={<DriverLoadPage />} />
         <Route path="documents" element={<DocumentsPage />} />
-        <Route path="drivers" element={<PlaceholderPage />} />
-        <Route path="vehicles" element={<PlaceholderPage />} />
-        <Route path="communications" element={<PlaceholderPage />} />
-        <Route path="expenses" element={<PlaceholderPage />} />
-        <Route path="invoices" element={<PlaceholderPage />} />
-        <Route path="reports" element={<PlaceholderPage />} />
-        <Route path="settings" element={<CompanySettingsPage />} />
+        <Route element={<OfficeOnly />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="loads" element={<LoadsPage />} />
+          <Route path="loads/new" element={<NewLoadPage />} />
+          <Route path="loads/:id" element={<LoadDetailPage />} />
+          <Route path="quotes" element={<QuotesPage />} />
+          <Route path="customers" element={<CustomersPage />} />
+          <Route path="drivers" element={<PlaceholderPage />} />
+          <Route path="vehicles" element={<PlaceholderPage />} />
+          <Route path="communications" element={<PlaceholderPage />} />
+          <Route path="expenses" element={<PlaceholderPage />} />
+          <Route path="invoices" element={<PlaceholderPage />} />
+          <Route path="reports" element={<PlaceholderPage />} />
+          <Route path="settings" element={<CompanySettingsPage />} />
+        </Route>
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
