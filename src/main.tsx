cef1722cbdc3dbaabd5ceec55fa3867664278293
Steve Smith <150203199+Memphisslim74@users.noreply.github.com>
2026-07-29 23:26:21 -0500

    Load sample preview workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './relationships.css'
 import './tracker.css'
 import './legacy-operations.css'
 import './admin-workspace.css'
+import './sample-preview.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
